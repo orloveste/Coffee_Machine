9d0810c0333240d9081a9134fd6de103a8bddf86
orloveste <orlovevm@yahoo.com>
2020-05-25 13:08:04 +0300

    CoffeeMachine.java - add ingredients and make inventory

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,13 +1,22 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!");
+        Scanner reader = new Scanner(System.in);
+        /*need for one cup of coffe*/
+        int water = 200; // ml
+        int milk = 50; // ml
+        int coffeBeans = 15; // g
+
+        System.out.println("Write how many cups of coffee you will need:");
+        int amountOfCoffee = reader.nextInt();
+
+
+        System.out.println("For " + amountOfCoffee + " cups of coffee you will need:\n" +
+                amountOfCoffee * water + " ml of water\n" +
+                amountOfCoffee * milk + " ml of milk\n" +
+                amountOfCoffee * coffeBeans + " g of coffee beans");
     }
 }
