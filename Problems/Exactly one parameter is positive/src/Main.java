ced6cd898d60ea18779a7d420eb9d005d888cbc5
orloveste <orlovevm@yahoo.com>
2020-06-11 13:53:02 +0300

    Version 2 - Exactly one parameter is positive

--- Problems/Exactly one parameter is positive/src/Main.java
+++ Problems/Exactly one parameter is positive/src/Main.java
@@ -3,18 +3,12 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt(); //-1;
-        int b = scanner.nextInt(); //-1;
-        int c = scanner.nextInt(); //1;
-
-        if (a > 0 && b <= 0 && c <= 0) {
-            System.out.println(true);
-        } else if (a <= 0 && b > 0 && c <= 0) {
-            System.out.println(true);
-        } else if (a <= 0 && b <= 0 && c > 0) {
-            System.out.println(true);
-        } else {
-            System.out.println(false);
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            if (scanner.nextInt() > 0) {
+                count++;
+            }
         }
+        System.out.println(count == 1);
     }
 }
