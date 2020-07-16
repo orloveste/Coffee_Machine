ae60290ab5b40d5986aaaa0bbb99d54b41fc28c9
orloveste <orlovevm@yahoo.com>
2020-07-16 17:06:23 +0300

    Problems-Currencies.iml

---
+++ Problems/Currencies/src/Main.java
@@ -0,0 +1,20 @@
+    enum Currency {
+    USD(" — United States dollar"),
+    EUR(" — Euro"),
+    GBP(" — Pound sterling"),
+    RUB(" — Russian ruble"),
+    UAH(" — Ukrainian hryvnia"),
+    KZT(" — Kazakhstani tenge"),
+    CAD(" — Canadian dollar"),
+    JPY(" — Japanese yen"),
+    CNY(" — Chinese yuan");
+        private final String description;
+
+        Currency(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
--- Problems/The (un)-ordered sequence/src/Main.java
+++ Problems/The (un)-ordered sequence/src/Main.java
@@ -1,53 +1,5 @@
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        int num = reader.nextInt();
-        int checkNum = reader.nextInt();
-        boolean pass = false;
-
-        if (num <= checkNum) {
-            pass = isPass(reader, checkNum, pass);
-        }
-        else if (num >= checkNum) {
-            pass = isPass(reader, checkNum, pass);
-        }
-        System.out.println(pass);
-
-
-
-        /*Scanner reader = new Scanner(System.in);
-        int input = reader.nextInt();
-        int [] array = new int[input];
-        int counter = 0;
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] >= array[i - 1] ) {
-                counter++;
-            } else if (array[i] <= array[i - 1] ) {
-                counter++;
-            }
-        }
-        System.out.println(counter == array.length - 1);*/
-    }
-
-    private static boolean isPass(Scanner reader, int checkNum, boolean pass) {
-        int num;
-        num = checkNum;
-        do {
-            checkNum = reader.nextInt();
-            if (checkNum == ' ') {
-                break;
-            } else if (checkNum >= num) {
-                pass = true;
-                num = checkNum;
-            }
-        } while (pass == true);
-        return pass;
+        // put your code here
     }
 }
-
-//Failed test #3 of 11. Wrong answer  This is a sample test from the problem statement!
-// Test input: 1 2 5 5 2 3 0
-// Correct output: false
-// Your code output: true
