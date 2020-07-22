70081afa8030a030df19b9aac11713aaa36f9040
orloveste <orlovevm@yahoo.com>
2020-07-22 14:36:27 +0300

    Balance management

--- Problems/Car/src/Main.java
+++ Problems/Car/src/Main.java
@@ -2,5 +2,32 @@ class Car {
 
     int yearModel;
     String make;
-    int speed;
+    int speed/* = 0*/;
+
+    void accelerate() {
+        setSpeed(this.speed + 5);
+    }
+    void setSpeed(int newSpeed) {
+        this.speed = newSpeed;
+    }
+
+    void brake() {
+        if (this.speed >= 5) {
+            setSpeed(this.speed - 5);
+        }
+    }
+
+    /*public static void main(String[] args) {
+        Car test = new Car();
+        test.accelerate();
+        System.out.println(test.speed);
+        test.accelerate();
+        System.out.println(test.speed);
+        test.brake();
+        System.out.println(test.speed);
+        test.brake();
+        System.out.println(test.speed);
+        test.brake();
+        System.out.println(test.speed);
+    }*/
 }
